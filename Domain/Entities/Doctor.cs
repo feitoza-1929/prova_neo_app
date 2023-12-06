1bb6bac7addcba990b52605571ab14b0b5c5fa35
Feitoza <thiagobritofeitoza@gmail.com>
2023-12-06 17:20:16 -0300

    [FEAT]: created Doctor entity model
    To model the database I choose the code first approach
    using annotations to add constraints to create the database table

---
+++ Domain/Entities/Doctor.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities;
+
+public class Doctor
+{
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "Doctor CRM field is required")]
+    public CRM CRM { get; set; }
+    
+    [Required(ErrorMessage = "Doctor name field is required")]
+    [MaxLength(50, ErrorMessage = "Doctor name field cannot exceed 6 digits")]
+    public string Name { get; set; }
+}
