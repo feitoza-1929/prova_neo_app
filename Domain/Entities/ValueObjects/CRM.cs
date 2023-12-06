9e1393bcfb50bdfce1c57863f3df05797c9f2984
Feitoza <thiagobritofeitoza@gmail.com>
2023-12-06 17:30:11 -0300

    [FEAT]: created CRM value object
    this value object is a building block to our entity model Doctor

---
+++ Domain/Entities/ValueObjects/CRM.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+public class CRM
+{
+    [Required(ErrorMessage = "CRM UF field is required")]
+    public string UF { get; set; }
+
+    [Required(ErrorMessage = "CRM number field is required")]
+    [MaxLength(6, ErrorMessage = "CRM number field cannot exceed 6 digits")]
+    public int Number { get; set; }
+}
