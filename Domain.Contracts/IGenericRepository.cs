7ad0d779c5ae2ee06e2ed35a899f9f54c1dfce56
Feitoza <thiagobritofeitoza@gmail.com>
2023-12-07 16:19:49 -0300

    [FEAT] created a generic repository
    This was created cuz has the basic CRUD operations, and avoid code redundancy

---
+++ Domain.Contracts/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace Domain.Contracts;
+
+public interface IGenericRepository<TEntity>
+{
+    IQueryable<TEntity> FindAll(bool isTrackingChanges = false);
+    IQueryable<TEntity> FindByCondition(Expression<Func<TEntity, bool>> expression, bool isTrackingChanges = false);
+    void Create(TEntity entity);
+    void Delete(TEntity entity);
+    void Update(TEntity entity);
+}
---
+++ Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,35 @@
+
+using Domain.Contracts;
+using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+{
+    private readonly ApplicationDbContext _context;
+
+    public GenericRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IQueryable<TEntity> FindAll(bool isTrackingChanges = false) => 
+        isTrackingChanges is false
+        ? _context.Set<TEntity>().AsNoTracking() 
+        : _context.Set<TEntity>();
+
+    public IQueryable<TEntity> FindByCondition(System.Linq.Expressions.Expression<Func<TEntity, bool>> expression, bool isTrackingChanges = false) =>
+        isTrackingChanges is false
+        ? _context.Set<TEntity>().Where(expression).AsNoTracking()
+        : _context.Set<TEntity>().Where(expression);
+
+    public void Update(TEntity entity)
+        => _context.Set<TEntity>().Update(entity);
+
+    public void Create(TEntity entity)
+        => _context.Set<TEntity>().Add(entity);
+
+    public void Delete(TEntity entity)
+        => _context.Set<TEntity>().Remove(entity);
+}
