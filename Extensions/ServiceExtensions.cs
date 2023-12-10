d4ddb25d4fc460f97872aab8ef20876de761bec8
Feitoza <thiagobritofeitoza@gmail.com>
2023-12-10 08:08:46 -0300

    [CHORE]: added Authentications services extensions

--- Extensions/ServiceExtensions.cs
+++ Extensions/ServiceExtensions.cs
@@ -1,7 +1,15 @@
+using System.Text;
 using Domain.Contracts;
+using Domain.Entities;
 using Infrastructure.Context;
 using Infrastructure.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Services;
+using Services.Contracts;
+
 
 namespace ProvaNeoApp.Extensions;
 
@@ -9,7 +17,53 @@ public static class ServiceExtensions
 {
     public static void AddRepositoryManager(this IServiceCollection services) =>
         services.AddScoped<IRepositoryManager, RepositoryManager>();
+    
     public static void AddSqlContext(this IServiceCollection services, IConfiguration configuration) =>
         services.AddDbContext<ApplicationDbContext>(opts =>
         opts.UseSqlServer(configuration.GetConnectionString("SqlServer")));
+    
+    public static void AddServiceManager(this IServiceCollection services)
+    {
+        services.AddScoped<IServiceManager, ServiceManager>();
+    }
+
+    public static void AddIdentityConfig(this IServiceCollection services) 
+    {
+        var build = services.AddIdentity<User, IdentityRole>(options =>
+        {
+            options.Password.RequireDigit = true;
+            options.Password.RequireLowercase = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequiredLength = 10;
+            options.User.RequireUniqueEmail = false;
+        })
+        .AddEntityFrameworkStores<ApplicationDbContext>()
+        .AddDefaultTokenProviders();
+    }
+
+    public static void AddJWT(this IServiceCollection services, IConfiguration configuration)
+    {
+        var jwtSettings = configuration.GetSection("JwtSettings");
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options => 
+        {
+            options.TokenValidationParameters =
+            new()
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = jwtSettings["validIssuer"],
+                ValidAudience = jwtSettings["validAudience"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["secretKey"]))
+            };
+        });
+    }
 }
