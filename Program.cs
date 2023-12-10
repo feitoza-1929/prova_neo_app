acf687e4cb84f1ee37166692f583e62acff030e2
Feitoza <thiagobritofeitoza@gmail.com>
2023-12-10 13:59:53 -0300

    [CHORE]: added config to swagger generate xml files

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+using Microsoft.OpenApi.Models;
 using ProvaNeoApp.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,7 +11,13 @@ builder.Services.AddControllers()
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => 
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "ProvaNeoApp", Version = "v1" });
+
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 builder.Services.AddRouting(opt => opt.LowercaseUrls = true);
 builder.Services.AddAutoMapper(typeof(Program));
 
