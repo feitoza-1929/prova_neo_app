d79da79092f64f5cc70c222edceb6a17db4a8c41
Feitoza <thiagobritofeitoza@gmail.com>
2023-12-10 07:35:53 -0300

    [FEAT]: created Service Manager
    This service takes cares of services creation and DI

---
+++ Services.Contracts/IServiceManager.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Services.Contracts;
+
+public interface IServiceManager
+{
+    IAuthenticationService AuthenticationService { get; }
+}
---
+++ Services/ServiceManager.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Services.Contracts;
+
+namespace Services;
+
+public class ServiceManager : IServiceManager
+{
+    private Lazy<IAuthenticationService> _authenticationService;
+
+    public ServiceManager(IMapper mapper, UserManager<User> userManager, IConfiguration configuration)
+    {
+        _authenticationService = new Lazy<IAuthenticationService>(() => 
+            new AuthenticationService(mapper, userManager, configuration));
+    }
+    public IAuthenticationService AuthenticationService => _authenticationService.Value;
+}
