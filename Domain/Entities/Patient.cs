6a7635eb81b57e22d963015107c5fdcf076ec39b
Feitoza <thiagobritofeitoza@gmail.com>
2023-12-12 03:02:00 -0300

    [REFACTOR]: removed dataAnnotations from Patient entity

--- Domain/Entities/Patient.cs
+++ Domain/Entities/Patient.cs
@@ -1,21 +1,11 @@
-using System.ComponentModel.DataAnnotations;
 using Domain.Entities.ValueObjects;
 
 namespace Domain.Entities;
 
-public class Patient
+public class Patient : Entity
 {
-    public Guid Id { get; set; }
-
-    [Required(ErrorMessage = "Patient name field is required")]
     public string Name { get; set; }
-
-    [Required(ErrorMessage = "Patient age field is required")]
-    [MaxLength(3, ErrorMessage = "Patient age field cannot exceed 3 digits")]
     public int Age { get; set;}
-
-    [Required(ErrorMessage = "Patient document field is required")]
     public Document Document {get; set;}
-
     public ICollection<Appointment> Appointments { get; set; }
 }
