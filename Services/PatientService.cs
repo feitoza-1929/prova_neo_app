8fa03ff46ee6fe851af744dd3b7d378b398d6a44
Feitoza <thiagobritofeitoza@gmail.com>
2023-12-12 16:57:29 -0300

    [FEAT]: added dapper delete operation

--- Services/PatientService.cs
+++ Services/PatientService.cs
@@ -1,11 +1,10 @@
-using System.Security.Claims;
 using AutoMapper;
+using Dapper;
 using Domain.Contracts;
 using Domain.Entities;
 using FluentResults;
 using FluentValidation;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Services.Contracts;
 using Shared;
@@ -14,7 +13,17 @@ namespace Services;
 
 public class PatientService : GenericService<Patient, PatientCreateDto, PatientUpdateDto>,IPatientService
 {
-    public PatientService(IMapper mapper, IGenericRepository<Patient> repository, IValidator<Patient> validator)
+    private readonly ISqlConnectionFactory _connectionFactory;
+    public PatientService(IMapper mapper, IGenericRepository<Patient> repository, IValidator<Patient> validator, ISqlConnectionFactory connectionFactory)
         : base(mapper, repository, validator)
-    { }
+    { 
+        _connectionFactory = connectionFactory;
+    }
+
+    public override async Task<Result> DeleteAsync(Guid id)
+    {
+        using SqlConnection connection = _connectionFactory.Create();
+        await connection.ExecuteAsync("DELETE FROM Patients WHERE Id = @Id", new { Id = id });
+        return Result.Ok();
+    }
 }
