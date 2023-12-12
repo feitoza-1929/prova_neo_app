1f10a4e27308dac6458fb7400b62d7993edff5b6
Feitoza <thiagobritofeitoza@gmail.com>
2023-12-12 16:56:47 -0300

    [REFACTOR]: removed dapper from generic repo

--- Infrastructure/Repositories/GenericRepository.cs
+++ Infrastructure/Repositories/GenericRepository.cs
@@ -9,12 +9,10 @@ namespace Infrastructure.Repositories;
 public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
 {
     private readonly ApplicationDbContext _context;
-    private readonly ISqlConnectionFactory _connectionFactory;
 
-    public GenericRepository(ApplicationDbContext context, ISqlConnectionFactory connectionFactory)
+    public GenericRepository(ApplicationDbContext context)
     {
         _context = context;
-        _connectionFactory = connectionFactory;
     }
 
     public IQueryable<TEntity> FindAll(bool isTrackingChanges = false) => 
@@ -27,25 +25,13 @@ public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEnt
         ? _context.Set<TEntity>().Where(expression).AsNoTracking()
         : _context.Set<TEntity>().Where(expression);
 
-    public IEnumerable<TEntity> FindById(Guid id, string entityName) 
-    {
-        using SqlConnection connection = _connectionFactory.Create();
-        
-        var result = connection.Query<TEntity>($"SELECT * FROM {entityName} WHERE Id = @id", new { id });
-        
-        return result;
-    }
     public void Update(TEntity entity)
         => _context.Set<TEntity>().Update(entity);
     public void Create(TEntity entity)
         => _context.Set<TEntity>().Add(entity);
 
-    public async Task Delete(Guid id)
-    {
-        using SqlConnection connection = _connectionFactory.Create();
-        var sql = $"DELETE {nameof(TEntity)} WHERE Id = @Id";
-        await connection.ExecuteAsync(sql, new { id });
-    }
+    public async Task Delete(Guid id) => 
+        _context.Set<TEntity>().Remove(await _context.Set<TEntity>().FindAsync(id));
 
     public async Task SaveAsync() => await _context.SaveChangesAsync();
 }
