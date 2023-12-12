4d4f0c59ee7cd58c24986b4a9cbf6d6ddd50cd97
Feitoza <thiagobritofeitoza@gmail.com>
2023-12-12 03:03:44 -0300

    [FEAT]: mapped DTOs to our entities

--- AutoMapper.cs
+++ AutoMapper.cs
@@ -1,13 +1,34 @@
 using AutoMapper;
 using Domain.Entities;
+using Services.Contracts;
+using Shared;
 using Shared.DTOs;
 
 namespace ProvaNeoApp;
 
 public class AutoMapper : Profile
 {
-    public AutoMapper()
+    public AutoMapper(IServiceManager serviceManager)
     {
-        CreateMap<UserRegistrationDto, User>();
+        CreateMap<Doctor, DoctorResponseDto>();
+        CreateMap<DoctorUpdateDto, Doctor>();
+        CreateMap<DoctorCreateDto, Doctor>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(x => serviceManager.UserVerifyService.GetUserId()))
+            .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(x => DateTime.UtcNow));
+
+        CreateMap<Patient, PatientResponseDto>();
+        CreateMap<PatientUpdateDto, Patient>();
+        CreateMap<PatientCreateDto, Patient>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(x => serviceManager.UserVerifyService.GetUserId()))
+            .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(x => DateTime.UtcNow));
+
+        CreateMap<Appointment, AppointmentResponseDto>();
+        CreateMap<AppointmentUpdateDto, Appointment>();
+        CreateMap<AppointmentCreateDto, Appointment>()
+           .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(x => DateTime.UtcNow))
+           .ForMember(dest => dest.State, opt => opt.MapFrom(x => AppointmentStates.SCHEDULED));
+
+        CreateMap<UserCreateDto, User>();
     }
+       
 }
