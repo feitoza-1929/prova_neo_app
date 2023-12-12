8fc3c389562692d324edfb6311b4fd0a9d7116b8
Feitoza <thiagobritofeitoza@gmail.com>
2023-12-12 06:24:53 -0300

    [REFACTOR]: added User property to relationship

--- Domain/Entities/Entity.cs
+++ Domain/Entities/Entity.cs
@@ -4,5 +4,6 @@ public abstract class Entity
 {
     public Guid Id { get; set; }
     public Guid UserId { get; set; }
+    public User User { get; set; }
     public DateTime CreatedAt { get; set; }
 }
