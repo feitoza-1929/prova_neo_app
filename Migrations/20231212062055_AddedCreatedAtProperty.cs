611bf9a52e3e188839411f2bd11bd3f478e18f86
Feitoza <thiagobritofeitoza@gmail.com>
2023-12-12 06:20:26 -0300

    [CHORE]: added migration AddedCreatedAtProperty

---
+++ Migrations/20231212062055_AddedCreatedAtProperty.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ProvaNeoApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCreatedAtProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "20144c8a-3543-44a9-b0f0-ca84603128a2");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "4d73109f-44f6-4c4f-93c9-33d39507d4cd");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Patients",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Doctors",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Doctors",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "16c1930b-f63a-4835-a9ac-5bd46e09378f", null, "Patient", "PATIENT" },
+                    { "19cbbb86-c6d1-4b3c-ac7b-5476a823391a", null, "Doctor", "DOCTOR" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "16c1930b-f63a-4835-a9ac-5bd46e09378f");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "19cbbb86-c6d1-4b3c-ac7b-5476a823391a");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Patients");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Doctors");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Doctors",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "20144c8a-3543-44a9-b0f0-ca84603128a2", null, "Patient", "PATIENT" },
+                    { "4d73109f-44f6-4c4f-93c9-33d39507d4cd", null, "Doctor", "DOCTOR" }
+                });
+        }
+    }
+}
