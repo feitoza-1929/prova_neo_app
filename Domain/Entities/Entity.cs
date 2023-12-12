b8a2692831e7b90056680580b4b1d7d2d515c5ad
Feitoza <thiagobritofeitoza@gmail.com>
2023-12-12 07:48:44 -0300

    [REFACTOR]: removed UserId property

--- Domain/Entities/Entity.cs
+++ Domain/Entities/Entity.cs
@@ -3,7 +3,5 @@ namespace Domain.Entities;
 public abstract class Entity
 {
     public Guid Id { get; set; }
-    public Guid UserId { get; set; }
-    public User User { get; set; }
     public DateTime CreatedAt { get; set; }
 }
